01ab48d83ebd7578ca5d7d04bf4abfab918d819e
Zuber8960 <zuberahmad8960@gmail.com>
2022-12-01 00:11:17 +0530

    routes_and_filter

--- main.js
+++ main.js
@@ -1,32 +1,21 @@
 
 const express= require('express');
-
 const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded());
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
 
-app.use('/add-product',(req, res, next) => {
-    console.log('the "add product" page!');
-    res.send(`<body><form action="/product" method="POST"><input type="text" name="title"><input type="text" name="title"><button type="submit">Add Product</button></form></body>`)
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.use('/admin',adminRouter);
+
+app.use(shopRouter);
+
+app.use((req, res, next)=>{
+    res.status(404).send('<h1>Error 404 : Page not found !</h1>')
 })
-
-app.use('/product',(req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/about',(req ,res, next) =>{
-    console.log('about page');
-    res.send('<h1>This is a about page!</h1>')
-})
-
-app.use('/',(req ,res, next) =>{
-    res.send('<h1>Hello from Express!</h1>')
-})
-
-
 
 
 app.listen(5000);
